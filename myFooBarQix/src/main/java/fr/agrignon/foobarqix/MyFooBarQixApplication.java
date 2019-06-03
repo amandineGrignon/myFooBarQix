e9fc9f281b8fa3bec9898c628a4e7c7d9004f5d6
amandineGrignon <46331344+amandineGrignon@users.noreply.github.com>
2019-06-03 19:16:08 +0200

    Initial commit
    Code generated with Eclipse Spring Boot

---
+++ myFooBarQix/src/main/java/fr/agrignon/foobarqix/MyFooBarQixApplication.java
@@ -0,0 +1,13 @@
+package fr.agrignon.foobarqix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFooBarQixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFooBarQixApplication.class, args);
+	}
+
+}
---
+++ myFooBarQix/src/test/java/fr/agrignon/foobarqix/MyFooBarQixApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.agrignon.foobarqix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyFooBarQixApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
