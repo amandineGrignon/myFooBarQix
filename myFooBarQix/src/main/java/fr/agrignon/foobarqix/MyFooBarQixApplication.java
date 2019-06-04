7913c4a800bdc88719df229b38923c8b28b57892
amandineGrignon <46331344+amandineGrignon@users.noreply.github.com>
2019-06-04 17:33:10 +0200

    Retrieving arguments from the main method

--- myFooBarQix/src/main/java/fr/agrignon/foobarqix/MyFooBarQixApplication.java
+++ myFooBarQix/src/main/java/fr/agrignon/foobarqix/MyFooBarQixApplication.java
@@ -8,6 +8,18 @@ public class MyFooBarQixApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFooBarQixApplication.class, args);
+		
+		if (args.length > 0) {
+			for (String arg : args) {
+				// Call compute method from FooBarQix
+			    String result = FooBarQix.compute(arg);
+			    
+			    System.out.format("%s => %s%n", arg, result);	
+			}
+		} else {
+			System.err.println("Argument is missing.");
+	        System.exit(1);
+		}
 	}
 
 }
