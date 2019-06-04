05e4fcfd1752930956bdef72b90a7a80c870a807
amandineGrignon <46331344+amandineGrignon@users.noreply.github.com>
2019-06-04 16:04:34 +0200

    Refactoring code by adding a map

--- myFooBarQix/src/main/java/fr/agrignon/foobarqix/FooBarQix.java
+++ myFooBarQix/src/main/java/fr/agrignon/foobarqix/FooBarQix.java
@@ -1,19 +1,35 @@
 package fr.agrignon.foobarqix;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class FooBarQix {
 
+	private static final Map<Integer, String> FOOBARQIX_RULES;
+	static {
+        // Using a LinkedHashMap to keep the insertion order
+        Map<Integer, String> map = new LinkedHashMap<>();
+        map.put(3, "Foo");
+        map.put(5, "Bar");
 
+        // Unmodifiable object makes it thread-safe (only reads can occurs)
+        FOOBARQIX_RULES = Collections.unmodifiableMap(map);
+    }
+	
 	public static String compute(String source) {
 		
-		// to build the string
+		// To build the string
         StringBuilder sb = new StringBuilder();
 		
+        // To convert the source to integer
 		int number = Integer.parseInt(source);
-		
-		if (number % 3 == 0)
-			sb.append("Foo");
-		else if (number % 5 == 0)
-			sb.append("Bar");
+	
+		// "is divisible by" rules
+		for (Map.Entry<Integer, String> entry : FOOBARQIX_RULES.entrySet()) {
+			if ((number % entry.getKey()) == 0)
+				sb.append(entry.getValue());
+		}
 		
 		return sb.toString();
 	}
