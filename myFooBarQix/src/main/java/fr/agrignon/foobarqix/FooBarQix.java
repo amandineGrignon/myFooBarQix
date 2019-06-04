acdbf107c91b77e2e88f71ae30a59d25f37dba39
amandineGrignon <46331344+amandineGrignon@users.noreply.github.com>
2019-06-04 15:36:52 +0200

    compute_ShouldWriteFoo_WhenNumberIsDivisibleBy3 : test OK

--- myFooBarQix/src/main/java/fr/agrignon/foobarqix/FooBarQix.java
+++ myFooBarQix/src/main/java/fr/agrignon/foobarqix/FooBarQix.java
@@ -3,6 +3,12 @@ package fr.agrignon.foobarqix;
 public class FooBarQix {
 
 	public String compute(String number) {
+		
+		int nb = Integer.parseInt(number);
+		
+		if (nb % 3 == 0)
+			return "Foo";
+		
 		return null;
 	}
 	
